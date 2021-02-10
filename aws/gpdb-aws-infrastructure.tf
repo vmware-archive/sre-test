559a868edb84839076e884d85ff25dbe150bc6db
Vivek Mandal <vivek.mandal@capgemini.com>
2021-02-10 11:17:48 +0000

    Initial Commit

--- aws/gpdb-aws-infrastructure.tf
+++ aws/gpdb-aws-infrastructure.tf
@@ -1,18 +1,24 @@
+#terraform script to setup Greenplum Cluster
+
+#provider details
 provider "aws" {
   region = var.aws_region
 }
 
-resource "tls_private_key" "gp_dev" {
+#create private key
+resource "tls_private_key" "gp_prod_private_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
 
-resource "aws_key_pair" "gp_dev" {
+#create aws key-pair
+resource "aws_key_pair" "gp_prod_key_pair" {
   key_name   = var.dwcluster_name
-  public_key = tls_private_key.gp_dev.public_key_openssh
+  public_key = tls_private_key.gp_prod_private_key.public_key_openssh
 }
 
-resource "aws_vpc" "gp_dev" {
+#create VPC network
+resource "aws_vpc" "gp_prod_vpc" {
   enable_dns_support = true
   enable_dns_hostnames = true
 
@@ -24,14 +30,14 @@ resource "aws_vpc" "gp_dev" {
     Name = var.dwcluster_name
   }
 }
-
-resource "aws_subnet" "gp_dev" {
-  vpc_id = aws_vpc.gp_dev.id
-
-  cidr_block = cidrsubnet(aws_vpc.gp_dev.cidr_block, 4, 1)
+#create subnet
+resource "aws_subnet" "gp_prod_subnet" {
+  vpc_id = aws_vpc.gp_prod_vpc.id
+  availability_zone = var.aws_zone
+  cidr_block = cidrsubnet(aws_vpc.gp_prod_vpc.cidr_block, 4, 1)
   map_public_ip_on_launch = true
 
-  ipv6_cidr_block = cidrsubnet(aws_vpc.gp_dev.ipv6_cidr_block, 8, 1)
+  ipv6_cidr_block = cidrsubnet(aws_vpc.gp_prod_vpc.ipv6_cidr_block, 8, 1)
   assign_ipv6_address_on_creation = true
 
   tags = {
@@ -39,38 +45,42 @@ resource "aws_subnet" "gp_dev" {
   }
 }
 
-resource "aws_internet_gateway" "gp_dev" {
-  vpc_id = aws_vpc.gp_dev.id
+#create VPC Internet Gateway
+resource "aws_internet_gateway" "gp_prod_gateway" {
+  vpc_id = aws_vpc.gp_prod_vpc.id
   tags = {
     Name = var.dwcluster_name
   }
 }
 
-resource "aws_default_route_table" "gp_dev" {
-  default_route_table_id = aws_vpc.gp_dev.default_route_table_id
+#create default VPC routing table
+resource "aws_default_route_table" "gp_prod_route_table" {
+  default_route_table_id = aws_vpc.gp_prod_vpc.default_route_table_id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gp_dev.id
+    gateway_id = aws_internet_gateway.gp_prod_gateway.id
   }
 
   route {
     ipv6_cidr_block = "::/0"
-    gateway_id = aws_internet_gateway.gp_dev.id
+    gateway_id = aws_internet_gateway.gp_prod_gateway.id
   }
   tags = {
     Name = var.dwcluster_name
   }
 }
 
-resource "aws_route_table_association" "gp_dev" {
-  subnet_id      = aws_subnet.gp_dev.id
-  route_table_id = aws_default_route_table.gp_dev.id
+#create an association between a route table and a subnet 
+resource "aws_route_table_association" "gp_prod_rt_association" {
+  subnet_id      = aws_subnet.gp_prod_subnet.id
+  route_table_id = aws_default_route_table.gp_prod_route_table.id
 }
 
-resource "aws_security_group" "gp_dev" {
+#create security group
+resource "aws_security_group" "gp_prod_sec_group" {
   name = var.dwcluster_name
-  vpc_id = aws_vpc.gp_dev.id
+  vpc_id = aws_vpc.gp_prod_vpc.id
   ingress {
     from_port = 22
     to_port = 22
@@ -113,49 +123,60 @@ resource "aws_security_group" "gp_dev" {
     ipv6_cidr_blocks = ["::/0"]
   }
   tags = {
-    Name = "${var.dwcluster_name}_jumpbox"
+    Name = "${var.dwcluster_name}"
   }
 }
 
-resource "aws_instance" "gp_dev_jumpbox" {
-
-  ami = var.ami
-  instance_type = "t2.large"
-
-  key_name = aws_key_pair.gp_dev.key_name
-
-  subnet_id = aws_subnet.gp_dev.id
-
-  ipv6_address_count = 1
-
-  vpc_security_group_ids = [aws_security_group.gp_dev.id]
-
-  depends_on = [aws_internet_gateway.gp_dev]
-
-  tags = {
-    Name = "${var.dwcluster_name}_jumpbox"
-  }
-}
-
-resource "aws_instance" "gp_dev_dwcoordinator" {
-
-  count = var.dwcoordinator_instance_count
-
+#create master instance
+resource "aws_instance" "gp_prod_dwcoordinator" {
   ami = var.ami
   instance_type = var.dwcoordinator_instance_type
 
-  key_name = aws_key_pair.gp_dev.key_name
+  key_name = aws_key_pair.gp_prod_key_pair.key_name
 
-  subnet_id = aws_subnet.gp_dev.id
+  subnet_id = aws_subnet.gp_prod_subnet.id
 
   ipv6_address_count = 1
 
-  vpc_security_group_ids = [aws_security_group.gp_dev.id]
+  vpc_security_group_ids = [aws_security_group.gp_prod_sec_group.id]
 
-  depends_on = [aws_internet_gateway.gp_dev]
+  depends_on = [aws_internet_gateway.gp_prod_gateway]
 
   tags = {
-    Name = format("${var.dwcluster_name}_mdw%d", count.index + 1)
+    Name = "${var.dwcluster_name}_mdw",
+	Description = "${var.dwcluster_name}"
+  }
+
+  root_block_device {
+    volume_size = "200"
+    volume_type = "gp2"
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_size = var.dwcoordinator_ebs_volume_size
+    volume_type = var.dwcoordinator_ebs_volume_type
+  }
+}
+
+#create standby master instance
+resource "aws_instance" "gp_prod_dwstandbycoordinator" {
+  ami = var.ami
+  instance_type = var.dwcoordinator_instance_type
+
+  key_name = aws_key_pair.gp_prod_key_pair.key_name
+
+  subnet_id = aws_subnet.gp_prod_subnet.id
+
+  ipv6_address_count = 1
+
+  vpc_security_group_ids = [aws_security_group.gp_prod_sec_group.id]
+
+  depends_on = [aws_internet_gateway.gp_prod_gateway]
+
+  tags = {
+    Name = "${var.dwcluster_name}_smdw",
+	Description = "${var.dwcluster_name}"
   }
 
   root_block_device {
@@ -170,25 +191,27 @@ resource "aws_instance" "gp_dev_dwcoordinator" {
   }
 }
 
-resource "aws_instance" "gp_dev_dwdatanode" {
+#create segments hosts instances
+resource "aws_instance" "gp_prod_dwdatanode" {
 
   count = var.dwdatanode_instance_count
 
   ami = var.ami
   instance_type = var.dwdatanode_instance_type
 
-  key_name = aws_key_pair.gp_dev.key_name
+  key_name = aws_key_pair.gp_prod_key_pair.key_name
 
-  subnet_id = aws_subnet.gp_dev.id
+  subnet_id = aws_subnet.gp_prod_subnet.id
 
   ipv6_address_count = 1
 
-  vpc_security_group_ids = [aws_security_group.gp_dev.id]
+  vpc_security_group_ids = [aws_security_group.gp_prod_sec_group.id]
 
-  depends_on = [aws_internet_gateway.gp_dev]
+  depends_on = [aws_internet_gateway.gp_prod_gateway]
 
   tags = {
-    Name = format("${var.dwcluster_name}_sdw%d", count.index + 1)
+    Name = format("${var.dwcluster_name}_sdw%d", count.index + 1),
+	Description = "${var.dwcluster_name}"
   }
 
   root_block_device {
@@ -215,30 +238,40 @@ resource "aws_instance" "gp_dev_dwdatanode" {
   }
 }
 
-output "gp_dev_jumpbox-public-IPv4" {
-  value = aws_instance.gp_dev_jumpbox.public_ip
+#Outputs
+
+output "gp_prod_dwcoordinators-public-IPv4" {
+  value = aws_instance.gp_prod_dwcoordinator.public_ip
+}
+output "gp_prod_dwstandbycoordinators-public-IPv4" {
+  value = aws_instance.gp_prod_dwstandbycoordinator.public_ip
 }
 
-output "gp_dev_dwcoordinators-public-IPv4" {
-  value = aws_instance.gp_dev_dwcoordinator[*].public_ip
+output "gp_prod_dwcoordinators-private-IPv4" {
+  value = aws_instance.gp_prod_dwcoordinator.private_ip
 }
 
-output "gp_dev_dwcoordinators-private-IPv4" {
-  value = aws_instance.gp_dev_dwcoordinator[*].private_ip
+output "gp_prod_dwstandbycoordinators-private-IPv4" {
+  value = aws_instance.gp_prod_dwstandbycoordinator.private_ip
 }
 
-output "gp_dev_dwdatanodes-public-IPv4" {
-  value = aws_instance.gp_dev_dwdatanode[*].public_ip
+output "gp_prod_dwcoordinators-private-IPv6" {
+  value = aws_instance.gp_prod_dwcoordinator.ipv6_addresses[0]
 }
 
-output "gp_dev_dwdatanodes-private-IPv4" {
-  value = aws_instance.gp_dev_dwdatanode[*].private_ip
+output "gp_prod_dwstandbycoordinators-private-IPv6" {
+  value = aws_instance.gp_prod_dwstandbycoordinator.ipv6_addresses[0]
 }
 
-output "gp_dev_dwcoordinators-private-IPv6" {
-  value = aws_instance.gp_dev_dwcoordinator[*].ipv6_addresses[0]
+output "gp_prod_dwdatanodes-public-IPv4" {
+  value = aws_instance.gp_prod_dwdatanode[*].public_ip
 }
 
-output "gp_dev_dwdatanodes-private-IPv6" {
-  value = aws_instance.gp_dev_dwdatanode[*].ipv6_addresses[0]
+output "gp_prod_dwdatanodes-private-IPv4" {
+  value = aws_instance.gp_prod_dwdatanode[*].private_ip
 }
+
+output "gp_prod_dwdatanodes-private-IPv6" {
+  value = aws_instance.gp_prod_dwdatanode[*].ipv6_addresses[0]
+}
+
--- aws/variables.tf
+++ aws/variables.tf
@@ -1,43 +1,68 @@
 variable "aws_region" {
+  description  = "AWS region to be chosen for provisioning"
+  type = string
   default = "ap-south-1"
 }
 
 variable "aws_zone" {
+  description  = "AWS AZ to be chosen for provisioning"
+  type = string
   default = "ap-south-1a"
 }
 
 variable "dwcluster_name" {
-  default = "gp_dev"
-}
-
-variable "dwcoordinator_instance_count" {
-  default = 2
+  description  = "Greenplum DW Cluster Name"
+  type = string
+  default = "gp_prod"
 }
 
 variable "dwdatanode_instance_count" {
-  default = 2
-}
-
-variable "dwcoordinator_ebs_volume_size" {
-  default = 500
-}
-
-variable "dwdatanode_ebs_volume_size" {
-  default = 2048
-}
-
-variable "dwdatanode_ebs_volume_type" {
-  default = "sc1"
+  description  = "Number of segment hosts"
+  type = number
+  default = 4
 }
 
 variable "dwcoordinator_instance_type" {
+  description  = "Master node instance type"
+  type = string
   default = "r5.xlarge"
 }
 
+
+variable "dwcoordinator_ebs_volume_size" {
+  description  = "Master node volume size"
+  type = number
+  default = 512
+}
+
+
+variable "dwcoordinator_ebs_volume_type" {
+  description  = "segment host node volume type"
+  type = string
+  default = "sc1"
+}
+
+variable "dwdatanode_ebs_volume_size" {
+  description  = "segment host node volume size"
+  type = number
+  default = 512
+}
+
+variable "dwdatanode_ebs_volume_type" {
+  description  = "segment host node volume type"
+  type = string
+  default = "sc1"
+}
+
 variable "dwdatanode_instance_type" {
-  default = "r5.4xlarge"
+  description  = "segment host node instance type"
+  type = string
+  default = "r5.xlarge"
 }
 
 variable "ami" {
+  description  = "AWS AMI to be used for provisioning instances"
+  type = string
   default = "ami-026f33d38b6410e30" # ap-south-1 - CentOS 7 (x86_64) - with Updates HVM
 }
+
