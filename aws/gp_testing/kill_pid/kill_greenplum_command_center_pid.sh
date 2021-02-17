af365d2f9e5d49f0892715663259885a8433a32a
gpadmin <gpadmin@mdw_ipv4>
2021-02-17 16:52:35 +0000

    4th commit

---
+++ aws/gp_testing/kill_pid/kill_greenplum_command_center_pid.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo '====================Start of test=================='
+
+
+echo '====================kill gpcc pid on mdw======================'
+
+source /usr/local/greenplum-cc-6.3.0/gpcc_path.sh
+
+ps -ef| grep gpcc| grep -v grep
+
+ps -ef| grep gpcc| grep -v grep| awk {'print $2'}| xargs kill
+
+
+echo '======================check the gpcc process id and last log in gpperfmon db======================'
+
+ps -ef| grep gpcc| grep -v grep
+
+psql gpperfmon -c "select max(ctime) from gpmetrics.gpcc_system_history;"
+sleep 100
+
+echo '====================Recover gpcc by starting gpcc========================'
+
+gpcc start
+
+sleep 100
+
+echo '====================Check if gpcc process started and check the DB last log================'
+
+ps -ef| grep gpcc| grep -v grep
+
+psql gpperfmon -c "select max(ctime) from gpmetrics.gpcc_system_history;"
+
+
+echo 'End of test'
+
+
---
+++ aws/gp_testing/kill_pid/kill_mirror_pid.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo '====================Start of test=================='
+
+
+echo '====================connect to sdw1 and kill one primary segment======================'
+
+gpssh -h sdw1_ipv4 "ps -ef| grep 'mirror/gpseg3'| grep -v grep| awk {'print $2'}| xargs kill"
+
+sleep 100
+
+echo '======================check the failed segment======================'
+
+gpstate
+
+psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
+
+echo '====================Recover failed segment incremental========================'
+
+gprecoverseg -a
+
+sleep 100
+
+echo '====================Check if all segments are up and go ahead to rebalance================'
+
+psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
+
+echo 'End of test'
+
+psql -c "create table tab1 as select generate_series(1,1000000);"
+
+psql -c "select count(*), gp_segment_id from  tab1 group by 2;"
+
+psql -c "drop table tab1;"
+
