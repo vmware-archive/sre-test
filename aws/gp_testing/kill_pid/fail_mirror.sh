4b075a17eac5e40ff133ad0bf08c54a1d07a76c2
Vivek Mandal <vivek.mandal@capgemini.com>
2021-03-03 14:08:16 +0000

    added formatting

--- aws/gp_testing/kill_pid/fail_mirror.sh
+++ aws/gp_testing/kill_pid/fail_mirror.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-echo '====================Start of test=================='
+echo -e '\n==================== Start of test ==================\n'
 
 
-echo '====================connect to sdw1 and kill one primary segment======================'
+echo -e '==================== Connect to sdw1 and kill one primary segment ======================\n'
 
 gpssh -h sdw1_ipv4 "ps -ef| grep 'mirror/gpseg3'| grep -v grep| awk {'print $2'}| xargs kill"
 
 sleep 100
 
-echo '======================check the failed segment======================'
+echo -e '\n\n====================== Check the failed segment ======================\n'
 
 gpstate
 
 psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
 
-echo '====================Recover failed segment incremental========================'
+echo -e '\n\n==================== Recover failed segment incremental ========================\n'
 
 gprecoverseg -a
 
 sleep 100
 
-echo '====================Check if all segments are up and go ahead to rebalance================'
+echo -e '\n\n==================== Check if all segments are up and go ahead to rebalance ================\n'
 
 psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
 
-echo 'End of test'
+echo -e '\n\n================ End of test ================\n'
 
 psql -c "create table tab1 as select generate_series(1,1000000);"
 
 psql -c "select count(*), gp_segment_id from  tab1 group by 2;"
 
 psql -c "drop table tab1;"
+
 
--- aws/gp_testing/kill_pid/kill_greenplum_command_center_pid.sh
+++ aws/gp_testing/kill_pid/kill_greenplum_command_center_pid.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # this test is to check kill GPCC process and recover back
-echo '====================Start of test=================='
+echo -e '\n==================== Start of test ==================\n'
 
-
-echo '====================kill gpcc pid on mdw======================'
+echo -e '==================== Kill gpcc pid on mdw ======================\n'
 
 source /usr/local/greenplum-cc-6.3.0/gpcc_path.sh
 
@@ -12,26 +11,27 @@ ps -ef| grep gpcc| grep -v grep
 ps -ef| grep gpcc| grep -v grep| awk {'print $2'}| xargs kill
 
 
-echo '======================check the gpcc process id and last log in gpperfmon db======================'
+echo -e '\n\n====================== Check the gpcc process id and last log in gpperfmon db ======================\n'
 
 ps -ef| grep gpcc| grep -v grep
 
 psql gpperfmon -c "select max(ctime) from gpmetrics.gpcc_system_history;"
 sleep 100
 
-echo '====================Recover gpcc by starting gpcc========================'
+echo -e '\n\n==================== Recover gpcc by starting gpcc ========================\n'
 
 gpcc start
 
 sleep 100
 
-echo '====================Check if gpcc process started and check the DB last log================'
+echo -e '\n\n==================== Check if gpcc process started and check the DB last log ================\n'
 
 ps -ef| grep gpcc| grep -v grep
 
 psql gpperfmon -c "select max(ctime) from gpmetrics.gpcc_system_history;"
 
 
-echo 'End of test'
+echo -e '\n\n================ End of test ================\n'
+
 
 
