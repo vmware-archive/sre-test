a3311ba176134f4541f429648a21b5ccdb167856
vivek-cg <vivek.mandal@capgemini.com>
2021-03-11 07:52:16 +0000

    minor changes and fixes in fail_vm

--- aws/gp_testing/fail_vm/fail_mdw_vm.sh
+++ aws/gp_testing/fail_vm/fail_mdw_vm.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 # this script need to be run from smdw
-echo '====================Start of test=================='
+echo -e '\n==================== Start of test ==================\n'
 
 
-echo '====================connect to mdw and reboot server======================'
+echo -e '==================== Connect to mdw and reboot server ======================\n'
 
 gpssh -h mdw_ipv4 "sudo reboot"
 
 sleep 100
 
-echo '====================== check if GP DB is ======================'
+echo -e '\n\n====================== Check if GP DB is up and running ======================\n'
 
 gpssh -h mdw_ipv4 "gpstate"
 
-echo '==================== activate the standby master as acting master========================'
+echo -e '\n\n==================== Activate the standby master as acting master ========================\n'
 
 export PGPORT=5432
 gpactivatestandby -ad /data1/gpdb/master/gpseg-1
 
 sleep 60
 
-echo '====================Check if db is up and running normal with smdw as================'
+echo -e '\n\n==================== Check if db is up and running normal with smdw acting as master ================\n'
 
 gpssh -h mdw_ipv4 "mv /data1/gpdb/master/gpseg-1 /data1/gpdb/master/bkp_gpseg-1"
 
@@ -35,7 +35,7 @@ psql -c "select count(*), gp_segment_id from  tab1 group by 2;"
 psql -c "drop table tab1;"
 
 
-echo '==================== Recover back to orginal configuration ================'
+echo -e '\n\n==================== Recover back to orginal configuration ================\n'
 
 gpstop -am
 
@@ -45,16 +45,16 @@ mv /data1/gpdb/master/gpseg-1 /data1/gpdb/master/bkp_gpseg-1
 
 gpssh -h mdw_ipv4 "gpinitstandby -as smdw_ipv4"
 
+echo -e '\n\n==================== Check if all segments are up and go ahead to rebalance ================\n'
+
 gpssh -h mdw_ipv4 "gpstate"
 
-
-echo '====================Check if all segments are up and go ahead to rebalance================'
-
-echo 'End of test'
+echo -e '\n\n======================== End of test ====================\n'
 
 gpssh -h mdw_ipv4 "psql -c 'create table tab1 as select generate_series(1,1000000);'"
 
 gpssh -h mdw_ipv4 "psql -c 'select count(*), gp_segment_id from  tab1 group by 2;'"
 
 gpssh -h mdw_ipv4 "psql -c 'drop table tab1;'"
+
 
--- aws/gp_testing/fail_vm/fail_smdw_vm.sh
+++ aws/gp_testing/fail_vm/fail_smdw_vm.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-echo '====================Start of test=================='
+echo -e '\n==================== Start of test ==================\n'
 
 
-echo '====================connect to sdw1 and kill one primary segment======================'
+echo -e '==================== Connect to smdw and reboot it ======================\n'
 
 gpssh -h smdw_ipv4 "sudo reboot"
 
 sleep 100
 
-echo '======================check the failed segment======================'
+echo -e '\n\n====================== Check the failed standby master ======================\n'
 
 gpstate
 
 psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
 
-echo '==================== Recover failed stand by master ========================'
+echo -e '\n\n==================== Recover failed standby master ========================\n'
 
 gpinitstandby -r -M fast -a
 
@@ -27,15 +27,16 @@ sleep 100
 
 gpstate
 
-echo '====================Check if all segments are up and go ahead to rebalance================'
+echo -e '\n\n==================== Check if all standby master is up and running ================\n'
 
 psql -c "select * from gp_segment_configuration where role!=preferred_role or status = 'd'"
 
-echo 'End of test'
+echo -e '\n\n======================== End of test ====================\n'
 
 psql -c "create table tab1 as select generate_series(1,1000000);"
 
 psql -c "select count(*), gp_segment_id from  tab1 group by 2;"
 
 psql -c "drop table tab1;"
+
 
